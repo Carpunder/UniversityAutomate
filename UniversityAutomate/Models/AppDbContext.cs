918cb30aa06b07da2553fd4656425398561360ba
pavel.prakapenka <pavel.prakapenka@LXIBY1093>
2021-12-23 14:43:05 +0300

    Add DbContext Setup Conection to Db

---
+++ UniversityAutomate/Models/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace UniversityAutomate.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+        }
+    }
+}
--- UniversityAutomate/Program.cs
+++ UniversityAutomate/Program.cs
@@ -1,7 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using UniversityAutomate.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<AppDbContext>(x => x.UseSqlServer("Server=LXIBY1093\\SQLEXPRESS;Database=UniversityDb;Trusted_Connection=True;"));
+
 
 var app = builder.Build();
 
